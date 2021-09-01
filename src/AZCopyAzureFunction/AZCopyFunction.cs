cd97624988be575d9dde8aed11a418da9689f85f
Greg Mardon <gregmardon@microsoft.com>
2021-09-01 10:36:43 -0600

    Initial commit of the AZCopy Azure function
    Not tested yet

---
+++ src/AZCopyAzureFunction/AZCopyFunction.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AZCopyAzureFunction
+{
+    public static class AZCopyFunction
+    {
+        [FunctionName("RunAZCopyBackup")]
+        public static void Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = Path.Combine(context.FunctionAppDirectory, "azcopy.exe");
+            process.StartInfo.Arguments = $"sync \"{config["Source"]}\" \"{config["Destination"]}\" ";
+            // Keep this False, is MUST !
+            process.StartInfo.UseShellExecute = false;
+            // Enabling Reading Application's Outputs
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            log.LogInformation(output);
+            process.WaitForExit();
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            if(process.ExitCode != 0)
+            {
+                log.LogWarning($"Invalid exit code: {process.ExitCode}");
+            }
+        }
+    }
+}
